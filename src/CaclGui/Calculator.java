9cafe43d5641f951fe1bbb1939f770888d4e3b6c
Dragon0344 <48781251+Dragon0344@users.noreply.github.com>
2020-10-14 08:58:26 -0700

    Started Calculator

--- src/CaclGui/Calculator.java
+++ src/CaclGui/Calculator.java
@@ -1,4 +1,16 @@
 package CaclGui;
 
+import javax.swing.*;
+
 public class Calculator {
+    private JButton Add;
+    private JPanel MainPanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
 }
