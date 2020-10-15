8a98e9cc1a1db9925cf06fdeb57f74d140daa9ed
Dragon0344 <48781251+Dragon0344@users.noreply.github.com>
2020-10-14 21:14:14 -0700

    Finished Calculator GUI(I think)

--- src/CaclGui/Calculator.java
+++ src/CaclGui/Calculator.java
@@ -5,12 +5,13 @@ import javax.swing.*;
 public class Calculator {
     private JButton Add;
     private JPanel MainPanel;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
+    private JButton Subtract;
+    private JButton Divide;
+    private JButton Multiply;
+    private JButton sqrt;
+    private JButton Power;
+    private JButton pi;
+    private JButton Eqauls;
+    private JButton Clear;
+    private JTextArea textArea1;
 }
